9651d313600c90fd11f92caef4814a0ee6ac091c
tranxuanbach0 <trantiger706@gmail.com>
2024-11-26 16:18:35 +0700

    session05 1

--- session05 1.cpp
+++ session05 1.cpp
@@ -1,17 +1,8 @@
 #include <stdio.h>
 
-int main(){
-	int target = 10;
-	int input;
-	do{
-		printf("hay nhap so dung\n");
-		scanf("%d",&input);
-		
-		if (input!=target){
-			printf("sai\n");
-		}
-	}
-     while (input!=target);
-	printf("dung",target); 
-	return 0;
+int main() {
+    for (int i = 100; i >= 1; i--) {
+        printf("%d ", i);
+    }
+    return 0;
 }
